10ea5607a80a6475efc81289550f05ea74183fed
Sergiusz <xbxbcvbxc@gmail.com>
2024-12-08 05:32:34 -0500

    My personal Kali Linux changes

---
+++ kali-config/common/includes.chroot/etc/profile.d/custom-shortcuts.sh
@@ -0,0 +1,39 @@
+# Alt + A / Alt + Shift + A
+xfconf-query --channel xfce4-keyboard-shortcuts --property "/xfwm4/custom/<Primary><Alt>Left" --reset --recursive
+xfconf-query --channel xfce4-keyboard-shortcuts --property "/xfwm4/custom/<Alt>A" --create --type string --set "left_workspace_key"
+xfconf-query -c xfce4-keyboard-shortcuts -p "/xfwm4/custom/<Alt><Shift>A" -n -t string -s "move_window_left_workspace_key"
+
+# Alt + D / Alt + Shift + D
+xfconf-query -c xfce4-keyboard-shortcuts -p "/xfwm4/custom/<Primary><Alt>Right" -r -R
+xfconf-query -c xfce4-keyboard-shortcuts -p "/xfwm4/custom/<Alt>D" -n -t string -s "right_workspace_key"
+xfconf-query -c xfce4-keyboard-shortcuts -p "/xfwm4/custom/<Alt><Shift>D" -n -t string -s "move_window_right_workspace_key"
+
+# Remove default: Ctrl + Alt {Up/Down}
+xfconf-query -c xfce4-keyboard-shortcuts -p "/xfwm4/custom/<Primary><Alt>Up" -r -R
+xfconf-query -c xfce4-keyboard-shortcuts -p "/xfwm4/custom/<Primary><Alt>Down" -r -R
+
+# Ctrl + Alt + Del
+xfconf-query -c xfce4-keyboard-shortcuts -p "/commands/custom/<Primary><Alt>Delete" -n -t string -s "xflock4"
+
+# Alt + Tab
+xfconf-query -c xfce4-keyboard-shortcuts -p "/xfwm4/custom/<Alt>Tab" -n -t string -s "cycle_windows_key"
+
+# Super + Left
+#xfconf-query -c xfce4-keyboard-shortcuts -p "/xfwm4/custom/<Super>Left" -n -t string -s "tile_left_key"
+# Alt + Left
+xfconf-query -c xfce4-keyboard-shortcuts -p "/xfwm4/custom/<Alt>Left" -n -t string -s "tile_left_key"
+
+# Super + Right
+#xfconf-query -c xfce4-keyboard-shortcuts -p "/xfwm4/custom/<Super>Right" -n -t string -s "tile_right_key"
+# Alt + Right
+xfconf-query -c xfce4-keyboard-shortcuts -p "/xfwm4/custom/<Alt>Right" -n -t string -s "tile_right_key"
+
+# Super + Up
+#xfconf-query -c xfce4-keyboard-shortcuts -p "/xfwm4/custom/<Super>Up" -n -t string -s "maximize_window_key"
+# Alt + Up
+xfconf-query -c xfce4-keyboard-shortcuts -p "/xfwm4/custom/<Alt>Up" -n -t string -s "maximize_window_key"
+
+# Super + Down
+#xfconf-query -c xfce4-keyboard-shortcuts -p "/xfwm4/custom/<Super>Down" -n -t string -s "hide_window_key"
+# Super + Down
+xfconf-query -c xfce4-keyboard-shortcuts -p "/xfwm4/custom/<Alt>Down" -n -t string -s "hide_window_key"
