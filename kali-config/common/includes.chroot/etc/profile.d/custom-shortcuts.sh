38cc7a455349b29527444bf7e6e8fd057abfbc82
Sergiusz <xbxbcvbxc@gmail.com>
2025-01-07 07:04:11 -0500

    Fix and remap shortcuts

--- kali-config/common/includes.chroot/etc/profile.d/custom-shortcuts.sh
+++ kali-config/common/includes.chroot/etc/profile.d/custom-shortcuts.sh
@@ -18,22 +18,28 @@ xfconf-query -c xfce4-keyboard-shortcuts -p "/commands/custom/<Primary><Alt>Dele
 # Alt + Tab
 xfconf-query -c xfce4-keyboard-shortcuts -p "/xfwm4/custom/<Alt>Tab" -n -t string -s "cycle_windows_key"
 
+# Remove <Super> key for xfce4-popup-whiskermenu
+xfconf-query -c xfce4-keyboard-shortcuts -p "/commands/custom/Super_L" -r -R
+
+# Super + A
+xfconf-query -c xfce4-keyboard-shortcuts -p "/commands/custom/<Super>A" -n -t string -s "xfce4-popup-whiskermenu"
+
 # Super + Left
 #xfconf-query -c xfce4-keyboard-shortcuts -p "/xfwm4/custom/<Super>Left" -n -t string -s "tile_left_key"
 # Alt + Left
-xfconf-query -c xfce4-keyboard-shortcuts -p "/xfwm4/custom/<Alt>Left" -n -t string -s "tile_left_key"
+#xfconf-query -c xfce4-keyboard-shortcuts -p "/xfwm4/custom/<Alt>Left" -n -t string -s "tile_left_key"
 
 # Super + Right
 #xfconf-query -c xfce4-keyboard-shortcuts -p "/xfwm4/custom/<Super>Right" -n -t string -s "tile_right_key"
 # Alt + Right
-xfconf-query -c xfce4-keyboard-shortcuts -p "/xfwm4/custom/<Alt>Right" -n -t string -s "tile_right_key"
+#xfconf-query -c xfce4-keyboard-shortcuts -p "/xfwm4/custom/<Alt>Right" -n -t string -s "tile_right_key"
 
 # Super + Up
 #xfconf-query -c xfce4-keyboard-shortcuts -p "/xfwm4/custom/<Super>Up" -n -t string -s "maximize_window_key"
 # Alt + Up
-xfconf-query -c xfce4-keyboard-shortcuts -p "/xfwm4/custom/<Alt>Up" -n -t string -s "maximize_window_key"
+#xfconf-query -c xfce4-keyboard-shortcuts -p "/xfwm4/custom/<Alt>Up" -n -t string -s "maximize_window_key"
 
 # Super + Down
 #xfconf-query -c xfce4-keyboard-shortcuts -p "/xfwm4/custom/<Super>Down" -n -t string -s "hide_window_key"
-# Super + Down
-xfconf-query -c xfce4-keyboard-shortcuts -p "/xfwm4/custom/<Alt>Down" -n -t string -s "hide_window_key"
+# Alt + Down
+#xfconf-query -c xfce4-keyboard-shortcuts -p "/xfwm4/custom/<Alt>Down" -n -t string -s "hide_window_key"
