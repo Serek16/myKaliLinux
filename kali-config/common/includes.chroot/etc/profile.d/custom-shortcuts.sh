70734a76cdb609c1f3c1ef5c19afe09520a0c390
Sergiusz <xbxbcvbxc@gmail.com>
2025-03-30 06:16:37 -0400

    Add mosquitto package. Minor tweaks and fixes

--- kali-config/common/includes.chroot/etc/profile.d/custom-shortcuts.sh
+++ kali-config/common/includes.chroot/etc/profile.d/custom-shortcuts.sh
@@ -1,14 +1,17 @@
+# List all shortcuts
+#xfconf-quert -c xfce4-keyboard-shortcuts -lv
+
 # Alt + A / Alt + Shift + A
-xfconf-query --channel xfce4-keyboard-shortcuts --property "/xfwm4/custom/<Primary><Alt>Left" --reset --recursive
 xfconf-query --channel xfce4-keyboard-shortcuts --property "/xfwm4/custom/<Alt>A" --create --type string --set "left_workspace_key"
-xfconf-query -c xfce4-keyboard-shortcuts -p "/xfwm4/custom/<Alt><Shift>A" -n -t string -s "move_window_left_workspace_key"
+xfconf-query -c xfce4-keyboard-shortcuts -p "/xfwm4/custom/<Shift><Alt>A" -n -t string -s "move_window_left_workspace_key"
 
 # Alt + D / Alt + Shift + D
-xfconf-query -c xfce4-keyboard-shortcuts -p "/xfwm4/custom/<Primary><Alt>Right" -r -R
 xfconf-query -c xfce4-keyboard-shortcuts -p "/xfwm4/custom/<Alt>D" -n -t string -s "right_workspace_key"
-xfconf-query -c xfce4-keyboard-shortcuts -p "/xfwm4/custom/<Alt><Shift>D" -n -t string -s "move_window_right_workspace_key"
+xfconf-query -c xfce4-keyboard-shortcuts -p "/xfwm4/custom/<Shift><Alt>D" -n -t string -s "move_window_right_workspace_key"
 
-# Remove default: Ctrl + Alt {Up/Down}
+# Remove default: Ctrl + Alt + {Left/Right/Up/Down}
+xfconf-query --channel xfce4-keyboard-shortcuts --property "/xfwm4/custom/<Primary><Alt>Left" --reset --recursive
+xfconf-query -c xfce4-keyboard-shortcuts -p "/xfwm4/custom/<Primary><Alt>Right" -r -R
 xfconf-query -c xfce4-keyboard-shortcuts -p "/xfwm4/custom/<Primary><Alt>Up" -r -R
 xfconf-query -c xfce4-keyboard-shortcuts -p "/xfwm4/custom/<Primary><Alt>Down" -r -R
 
